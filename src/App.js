56c4727b94e6d902aae914203b319b0ca2583165
konstantinos koliolios <kolioliosk@gmail.com>
2023-06-12 13:15:35 +0100

    about component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Home from './components/Home/Home';
+import About from './components/About/About';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
           </Route>
         </Routes>
       </Router>
