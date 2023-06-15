72c538927d863388233665ad904142909d3b39a9
konstantinos koliolios <kolioliosk@gmail.com>
2023-06-15 17:39:45 +0100

    changes

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,8 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore';
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,4 +18,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth();
+const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+
+export const signInWithGoogle = () => signInWithPopup(auth, provider);
---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters';
+import { getDocs, collection } from 'firebase/firestore';
+import { db } from '../../firebase';
+import './Portfolio.scss';
+
+const Portfolio = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const [portfolio, setPortfolio] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 3000);
+  }, []);
+
+  useEffect(() => {
+    getPortfolio();
+  }, []);
+
+  const getPortfolio = async () => {
+    const querySnapshot = await getDocs(collection(db, 'portfolio'));
+    console.log(querySnapshot);
+    setPortfolio(querySnapshot.docs.map((doc) => doc.data()));
+  };
+
+  const renderPortfolio = (portfolio) => {
+    return (
+      <div className="images-container">
+        {portfolio.map((port, idx) => {
+          return (
+            <div key={idx} className="image-box">
+              <img
+                src={port.image}
+                alt="portfolio cover"
+                className="portfolio-image"
+              />
+              <div className="content">
+                <p className="title">{port.name}</p>
+                <h4 className="description">{port.description}</h4>
+                <button
+                  className="button"
+                  onClick={() => {
+                    window.open(port.url);
+                  }}
+                >
+                  View
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={'Portfolio'.split('')}
+            index={15}
+          />
+        </h1>
+        <div>{renderPortfolio(portfolio)}</div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  );
+};
+
+export default Portfolio;
