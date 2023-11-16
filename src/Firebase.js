f10b3d8cfcd76a329ad9f597e3929c5717128f5a
konstantinos koliolios <kolioliosk@gmail.com>
2023-11-16 09:31:45 +0000

    changes in firebase database

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,18 +1,22 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore"
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBf96AcCHQBSHRdQv-5yuUoCTZCI7XZZ7w",
-  authDomain: "react-portfolio-9bbad.firebaseapp.com",
-  projectId: "react-portfolio-9bbad",
-  storageBucket: "react-portfolio-9bbad.appspot.com",
-  messagingSenderId: "134534460385",
-  appId: "1:134534460385:web:3bc424211d8d35bdd0d8a0"
+  apiKey: "AIzaSyC1Ygjg01xorwmrxFN6Gy-24IUTUEakNpQ",
+  authDomain: "portfolio-c8d71.firebaseapp.com",
+  projectId: "portfolio-c8d71",
+  storageBucket: "portfolio-c8d71.appspot.com",
+  messagingSenderId: "1069075674706",
+  appId: "1:1069075674706:web:524c230fb996086bb0918b",
+  measurementId: "G-3PJGTPLBDG"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const auth = getAuth();
 const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
